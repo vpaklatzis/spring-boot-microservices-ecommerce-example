d383e880e111297f747282751682432ba82ec4f2
vpaklatzis <vpaklatzis@gmail.com>
2022-05-04 03:10:25 +0300

    completed inventory-service and added minor change to OrderService

--- inventory-service/src/main/java/com/ecommerceapp/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/ecommerceapp/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.ecommerceapp.inventoryservice;
 
+import com.ecommerceapp.inventoryservice.model.Inventory;
+import com.ecommerceapp.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +13,19 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("Iphone_13");
+			inventory.setQuantity(100);
 
+			Inventory inventoryNew = new Inventory();
+			inventoryNew.setSkuCode("Iphone_14");
+			inventoryNew.setQuantity(20);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventoryNew);
+		};
+	}
 }
